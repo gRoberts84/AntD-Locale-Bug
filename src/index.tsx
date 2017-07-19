775c68c61786ecea218239b4118d61a8461f4bb3
Gavin Roberts <gavin.roberts@zecoenergy.com>
2017-07-19 10:16:57 +0100

    Updated for TreeSelect/TreeNode issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { LocaleProvider, Table } from 'antd';
+import { LocaleProvider, TreeSelect } from 'antd';
+
+const TreeNode = TreeSelect.TreeNode;
 
 import './index.css';
 
 import enGB from 'antd/lib/locale-provider/en_GB';
 
-const dataSource: any = [];
-
-const columns: any = [{
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-}, {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-}, {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-}];
-
 ReactDOM.render(
     <LocaleProvider locale={enGB}>
-        <Table dataSource={dataSource} columns={columns} />
+        <TreeSelect>
+            <TreeNode title="Test Node" />
+        </TreeSelect>
     </LocaleProvider>,
     document.getElementById('root') as HTMLElement
 );
